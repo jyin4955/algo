497217bdc5c04d620698f71449751cde04e9236a
jyin4955 <3218868762@qq.com>
2021-08-19 00:40:07 +1000

    20210818 Sydney Time 12:40PM

--- p12.py
+++ p12.py
@@ -13,3 +13,16 @@ def test3():
 
 def test4():
     l = list(range(1000))
+
+from timeit import Timer
+t1 = Timer("test1()", "from __main__ import test1")
+print("concat {} seconds".format(t1.timeit(number=1000)))
+
+t2 = Timer("test2()", "from __main__ import test2")
+print("append {} seconds".format(t2.timeit(number=1000)))
+
+t3 = Timer("test3()", "from __main__ import test3")
+print("comprehension {} seconds".format(t3.timeit(number=1000)))
+
+t4 = Timer("test4()", "from __main__ import test4")
+print("list range {} seconds".format(t4.timeit(number=1000)))
